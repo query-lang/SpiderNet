d83604ea00a77cb02780e46dfd37367f60f71b33
venvis <127123047+venvis@users.noreply.github.com>
2024-07-30 21:29:44 +0400

    Update  hash_map.py

---
+++ examples/DataTypes/hash_map.py
@@ -0,0 +1,24 @@
+
+from SpiderWeb import HashMap
+
+# Adding key and value to HashMap
+map=HashMap()
+map.add('storage','github')
+map.add('cost',22)
+
+# Get value of a respective key 
+key=map.value('cost') # returns 22
+print(key)
+
+# Get key of a respective value 
+key=map.value(22) # returns 'cost'
+print(key)
+
+#Getting list of all keys and list of all values 
+all_keys=map.keys()
+all_values=map.values() 
+
+#Getting the length of the HashMap
+length=len(map) #returns the length of the hashmap
+
+# The HashMap is also Iterable through the builtin ForEach Loop. Look at for_each.py
