536033d925b0302e9f748456bd42dbca9e043a1b
venvis <127123047+venvis@users.noreply.github.com>
2024-07-30 21:39:43 +0400

    Update str_type.py

---
+++ examples/DataTypes/str_type.py
@@ -0,0 +1,9 @@
+from SpiderWeb import Str 
+
+#You can either initialize a Str through declaration or leave it empty 
+integer=Str() #leave blank
+#or 
+integer=Str("Hello there!")
+
+# you can add numbers to the integer as well
+integer._("This is String") # adds 'This is String' to the string , default is "" when no string is initialized in the class
