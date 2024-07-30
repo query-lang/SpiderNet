f9ada7be900a009c348cfcb8bb990486f33b8719
venvis <127123047+venvis@users.noreply.github.com>
2024-07-30 21:36:52 +0400

    update int_type.py

---
+++ examples/DataTypes/int_type.py
@@ -0,0 +1,10 @@
+from SpiderWeb import Int 
+
+#You can either initialize an Integer through declaration or leave it empty 
+integer=Int() #leave blank
+#or 
+integer=Int(55)
+
+# you can add numbers to the integer as well
+integer._(22) # adds 22 to the number , default is 0 when no number is initialized in the class
+
